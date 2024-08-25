299eebab5d1b89cc45ad31a97186d8eb6ebf3af3
ronewa <ronewamaselesele200@gmail.com>
2024-08-25 20:05:43 +0200

    this file will be used for the logging out of the application.

---
+++ app/src/main/java/com/example/skyhigh_prototype/View/Logout.kt
@@ -0,0 +1,44 @@
+package com.example.skyhigh_prototype.View
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun Logout(navController: NavController){
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 50.dp)
+            .background(Color.LightGray),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedButton(onClick = { /*TODO*/ }) {
+            Text(text = "Logout")
+        }
+        Spacer(modifier = Modifier.weight(1f))
+
+        HorizontalDivider()
+        OutlinedButton(onClick = { /*TODO*/ }) {
+            Text(text = "Delete Account")
+        }
+        TextButton(onClick = { /*TODO*/ }) {
+            Text(text = "pause account")
+        }
+    }
+}
