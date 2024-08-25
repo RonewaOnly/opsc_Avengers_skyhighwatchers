62d8353d1b29d69f1a0948b9a1ea33b89193ccf0
ST10061533 <105481917+ST10061533@users.noreply.github.com>
2024-08-25 15:59:03 +0200

    Design changes

--- app/src/main/java/com/example/skyhigh_prototype/View/Login.kt
+++ app/src/main/java/com/example/skyhigh_prototype/View/Login.kt
@@ -2,30 +2,30 @@
 
 package com.example.skyhigh_prototype.View
 
-import android.annotation.SuppressLint
+
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -38,123 +38,250 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.skyhigh_prototype.R
 
-@SuppressLint("ResourceAsColor")
 @Composable
-fun Login(navController: NavController){
+fun Login(navController: NavController) {
+
     var username by remember {
         mutableStateOf("")
     }
-    var password by  remember {
+    var password by remember {
         mutableStateOf("")
     }
     val rememberMe = remember {
         mutableStateOf(false)
     }
-    //val context = LocalContext.current
+
 
     Column(
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
-            .border(width = 2.dp, color = Color(R.color.dark_blue))
-            .fillMaxHeight(1f)
+            .fillMaxSize()
             .background(Color.White)
     ) {
-        Spacer(modifier = Modifier.padding(10.dp))
-        Image(painter = painterResource(id = R.drawable.logo), contentDescription = "logo")
-        Spacer(modifier = Modifier.padding(50.dp))
-        TextField(
-            value = username,
-            onValueChange = {username = it},
-            label = {
-                Text(text = "Email")
-            },
-            leadingIcon = {
-                Icon(
-                    painter = painterResource(id = R.drawable.baseline_person_24),
-                    contentDescription = null
-                )
-            },
-            modifier = Modifier.width(350.dp)
-        )
-        Spacer(modifier = Modifier.padding(10.dp))
-        TextField(
-            value = password,
-            onValueChange = {password = it},
-            label = {
-                Text(text = "Password")
-            },
-            leadingIcon = {
-                Icon(
-                    painter = painterResource(id = R.drawable.password_key),
-                    contentDescription = null
-                )
-            },
-            trailingIcon = {
-                Icon(
-                    painter = painterResource(id = R.drawable.baseline_remove_red_eye_24),
-                    contentDescription = null,
-                    modifier = Modifier.clickable {
 
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.2f)
+                .background(Color.LightGray)
+        ) {
+            Spacer(modifier = Modifier.width(10.dp))
+            Image(
+                modifier = Modifier
+                    .width(400.dp)
+                    .height(350.dp)
+                    .padding(top = 30.dp),
+                painter = painterResource(id = R.drawable.sky_high_watchers_logo),
+                contentDescription = "logo"
+
+            )
+        }
+
+        Spacer(modifier = Modifier.height(25.dp))
+
+        Text(
+            text = "Hi, Welcome Back...",
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp,
+            modifier = Modifier.padding(20.dp, 0.dp)
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        // 70% height remaining
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.7f)
+                .background(Color.White)
+        ) {
+
+            TextField(
+                value = username,
+                onValueChange = { username = it },
+                label = {
+                    Text(text = "Email")
+                },
+
+                modifier = Modifier
+                    .width(400.dp)
+                    .padding(10.dp, 0.dp)
+            )
+
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = {
+                    Text(text = "Password")
+                },
+
+                trailingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_remove_red_eye_24),
+                        contentDescription = null,
+                        modifier = Modifier.clickable {
+
+                        }
+                    )
+                },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier
+                    .width(400.dp)
+                    .padding(10.dp, 80.dp)
+            )
+
+            //box for remember me and forgot password
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth() // Make the Box fill the entire width of the screen
+                    .padding(top = 140.dp) // Add top padding to position the elements as needed
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(), // Make the Row fill the entire width of the Box
+                    verticalAlignment = Alignment.CenterVertically, // Align elements vertically center within the Row
+                    horizontalArrangement = Arrangement.SpaceBetween // Space the elements between start and end
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically // Align checkbox and text vertically center
+                    ) {
+                        Checkbox(
+                            modifier = Modifier.padding(10.dp, 0.dp, 0.dp, 0.dp),
+                            checked = rememberMe.value,
+                            onCheckedChange = { rememberMe.value = it }
+                        )
+                        Spacer(modifier = Modifier.width(2.dp)) // Add some space between the checkbox and text
+                        Text(text = "Remember Me")
                     }
-                )
-            },
-            visualTransformation = PasswordVisualTransformation(),
-            modifier = Modifier.width(350.dp)
-        )
-        OutlinedButton(onClick = { navController.navigate("forgotpassword") }, shape = RoundedCornerShape(0)) {
-            Text(text = "Forgot password")
-        }
 
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start
-        ) {
-            Checkbox(checked = rememberMe.value, onCheckedChange = {rememberMe.value = it})
-            Text(text = "Remember Me")
-        }
-        
-        Button(onClick = { navController.navigate("homepage") }) {
-            Text(text = "Login")
-        }
-        HorizontalDivider()
-        Button(onClick = { /*TODO*/ }, contentPadding = PaddingValues(10.dp), modifier = Modifier.width(300.dp), shape = RectangleShape, colors = ButtonColors(
-            Color.White, Color.Black, Color.Blue,Color.LightGray), border = BorderStroke(1.dp, Color.DarkGray)) {
-            Icon(painter = painterResource(id = R.drawable.google_brands_solid), contentDescription = null ,modifier = Modifier
-                .clip(
-                    RoundedCornerShape(5)
-                )
-                .size(30.dp))
-            Spacer(modifier = Modifier.padding(5.dp))
-            Text(text = "Continue with Google")
-        }
-        Button(onClick = { /*TODO*/ },modifier = Modifier
-            .padding(10.dp, 5.dp)
-            .width(300.dp), shape = RectangleShape, colors = ButtonColors(
-            Color.White, Color.Black, Color.Blue,Color.LightGray), border = BorderStroke(1.dp, Color.DarkGray),
-        ) {
-            Icon(painter = painterResource(id = R.drawable.facebook_brands_solid), contentDescription = null,modifier = Modifier
-                .clip(
-                    RoundedCornerShape(5)
-                )
-                .size(30.dp)
-                .padding(0.dp))
-            Spacer(modifier = Modifier.padding(5.dp))
-            Text(text = "Continue with facebook")
-        }
-        Button(onClick = { navController.navigate("register") },modifier = Modifier
-            .padding(10.dp, 5.dp)
-            .width(300.dp), shape = RectangleShape, colors = ButtonColors(
-            Color.White, Color.Black, Color.Blue,Color.LightGray)
-        ) {
+                    TextButton(
+                        onClick = { navController.navigate("forgotPassword") },
+                        modifier = Modifier.padding(10.dp) // Add padding to the button if needed
+                    ) {
+                        Text(text = "Forgot Password?")
+                    }
+                }
+            }//End
 
-            Text(text = "Continue with email")
+            //button to login
+            Button(
+                onClick = { navController.navigate("homepage") },
+                shape = RoundedCornerShape(size = 10.dp),
+                modifier = Modifier
+                    .padding(70.dp, 220.dp, 0.dp, 0.dp)
+                    .width(250.dp)
+                    .height(50.dp)
+
+            ) {
+                Text(text = "Login")
+            }
+
+            HorizontalDivider(modifier = Modifier.padding(0.dp, 300.dp, 0.dp, 0.dp))
+
+            Column(
+                modifier = Modifier.align(Alignment.Center),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Spacer(modifier = Modifier.height(200.dp))
+
+                Text(
+                    text = "or continue with",
+                    textAlign = TextAlign.Center,
+                    fontSize = 18.sp,
+                    modifier = Modifier.padding(bottom = 10.dp) // Adjusted padding
+                )
+
+                //row for social media buttons
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp), // Optional padding to center the buttons within the column
+                    horizontalArrangement = Arrangement.Center, // Center buttons horizontally
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    // Google account button
+                    Button(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier
+                            .padding(10.dp, 5.dp)
+                            .width(150.dp),
+                        shape = RectangleShape,
+                        colors = buttonColors(
+                            Color.White, Color.Black, Color.Blue, Color.LightGray
+                        ),
+                        border = BorderStroke(1.dp, Color.DarkGray),
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.google_brands_solid),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .clip(RoundedCornerShape(5))
+                                .size(30.dp)
+                                .padding(0.dp)
+                        )
+                        Spacer(modifier = Modifier.width(5.dp))
+                        Text(text = "Google")
+                    }
+
+                    // Facebook account button
+                    Button(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier
+                            .padding(10.dp, 5.dp)
+                            .width(150.dp),
+                        shape = RectangleShape,
+                        colors = buttonColors(
+                            Color.White, Color.Black, Color.Blue, Color.LightGray
+                        ),
+                        border = BorderStroke(1.dp, Color.DarkGray),
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.facebook_brands_solid),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .clip(RoundedCornerShape(5))
+                                .size(30.dp)
+                                .padding(0.dp)
+                        )
+                        Spacer(modifier = Modifier.width(5.dp))
+                        Text(text = "Facebook")
+                    }
+
+                }
+                // Column for "Don't have an account?" and "Register" button
+                Row(
+                    modifier = Modifier.padding(top = 30.dp),
+                    horizontalArrangement = Arrangement.Center, // Center buttons horizontally
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    Text(
+                        text = "Don't have an account?",
+                        fontSize = 18.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
+                    )
+
+                    TextButton(
+                        onClick = { navController.navigate("register") },
+                    ) {
+                        Text(
+                            text = "Create Account",
+                            fontSize = 18.sp,
+                            color = Color.Blue,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                }
+            }
+
+
         }
 
     }
