b321ac0d69aa3bea27109921ac6980cad87133ef
ST10061533 <105481917+ST10061533@users.noreply.github.com>
2024-09-04 15:10:21 +0200

    Design changes

--- app/src/main/java/com/example/skyhigh_prototype/View/homepage.kt
+++ app/src/main/java/com/example/skyhigh_prototype/View/homepage.kt
@@ -1,48 +1,42 @@
+@file:Suppress("PackageName", "DEPRECATION")
+
 package com.example.skyhigh_prototype.View
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.InputChip
 import androidx.compose.material3.InputChipDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
@@ -54,26 +48,23 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.skyhigh_prototype.Model.MapboxViewModel
 import com.example.skyhigh_prototype.R
 import com.example.skyhigh_prototype.R.color.dark_blue
 import kotlinx.coroutines.launch
 
 
 @Composable
-fun Main(){
+fun Main() {
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope = rememberCoroutineScope()
     val rememberNavController = rememberNavController()
@@ -105,29 +96,35 @@ fun Main(){
             },
             floatingActionButton = {
                 ExtendedFloatingActionButton(
-                    text = { Text(text="World") },
-                    icon = { Icon(painter = painterResource(id = R.drawable.globe_solid), contentDescription = "world icon",modifier = Modifier.size(35.dp)) },
+                    text = { Text(text = "World") },
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.globe_solid),
+                            contentDescription = "world icon",
+                            modifier = Modifier.size(35.dp)
+                        )
+                    },
                     onClick = { rememberNavController.navigate("ViewMap") })
             }
         ) { contentPadding ->
             // Screen content
             NavHost(navController = rememberNavController, startDestination = "homepage") {
-                composable("homepage"){
+                composable("homepage") {
                     Homepage()
                 }
-                composable("settings"){
+                composable("settings") {
                     navOption()
                 }
-                composable("profile"){
+                composable("profile") {
                     Profile(rememberNavController)
                 }
-                composable("collection"){
+                composable("collection") {
                     PersonalCollection()
                 }
-                composable("ViewMap"){
-                    MapOption(rememberNavController )
+                composable("ViewMap") {
+                    MapOption(rememberNavController)
                 }
-                composable("logout"){
+                composable("logout") {
                     Logout(rememberNavController)
                 }
             }
@@ -137,95 +134,174 @@ fun Main(){
 }
 
 @Composable
-fun NavDrawer(navController: NavController){
-    Column(
-        modifier = Modifier.fillMaxHeight()
+fun NavDrawer(navController: NavController) {
+    
+    //column for entire page
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
     ) {
-        Image(painter = painterResource(id = R.drawable.logo), modifier = Modifier.fillMaxWidth() ,contentDescription =null, contentScale = ContentScale.FillWidth, colorFilter = ColorFilter.tint(Color.Red,
-            BlendMode.Difference))
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start,
+        
+        //box for logo image
+        Box (
             modifier = Modifier
-                .padding(10.dp)
-                .background(Color.LightGray)
                 .fillMaxWidth()
-        ) {
-            Icon(imageVector = Icons.Filled.Home, contentDescription = null)
-            OutlinedButton(onClick = { navController.navigate("Homepage")},modifier = Modifier.fillMaxWidth()
-                , shape = RectangleShape,
-                colors = ButtonColors(containerColor = Color.Transparent, contentColor = Color.Black, disabledContainerColor = Color.DarkGray, disabledContentColor = Color.Yellow),
-                border = BorderStroke(0.dp,Color.Transparent)) {
-                Text(text = "Home")
-            }
-        }
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start,
-            modifier = Modifier
-                .padding(10.dp)
+                .weight(0.25f)
                 .background(Color.LightGray)
-                .fillMaxWidth()
-        ) {
-            Icon(imageVector = Icons.Filled.Settings, contentDescription = null)
-            OutlinedButton(onClick = { navController.navigate("settings") },modifier = Modifier.fillMaxWidth()
-                , shape = RectangleShape,
-                colors = ButtonColors(containerColor = Color.Transparent, contentColor = Color.Black, disabledContainerColor = Color.DarkGray, disabledContentColor = Color.Yellow),
-                border = BorderStroke(0.dp,Color.Transparent)) {
-                Text(text = "Settings")
-            }
-        }
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start,
-            modifier = Modifier
-                .padding(10.dp)
-                .background(Color.LightGray)
-                .fillMaxWidth()
-        ) {
-            Icon(imageVector = Icons.Filled.Person, contentDescription = null)
-            OutlinedButton(onClick = { navController.navigate("profile") },modifier = Modifier.fillMaxWidth()
-                , shape = RectangleShape,
-                colors = ButtonColors(containerColor = Color.Transparent, contentColor = Color.Black, disabledContainerColor = Color.DarkGray, disabledContentColor = Color.Yellow),
-                border = BorderStroke(0.dp,Color.Transparent)) {
-                Text(text = "Profile")
-            }
-        }
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start,
-            modifier = Modifier
-                .padding(10.dp)
-                .background(Color.LightGray)
-                .fillMaxWidth()
-        ) {
-            Icon(imageVector = Icons.Filled.AddCircle, contentDescription = null)
-            OutlinedButton(onClick = { navController.navigate("collection") },modifier = Modifier.fillMaxWidth()
-                , shape = RectangleShape,
-                colors = ButtonColors(containerColor = Color.Transparent, contentColor = Color.Black, disabledContainerColor = Color.DarkGray, disabledContentColor = Color.Yellow),
-                border = BorderStroke(0.dp,Color.Transparent)) {
-                Text(text = "Collection")
-            }
-        }
-        Spacer(modifier = Modifier.weight(1f))
+        ){
 
-        HorizontalDivider()
-        OutlinedButton(onClick = { navController.navigate("logout") },modifier = Modifier
-            .fillMaxWidth()
-            , shape = RectangleShape,
-            colors = ButtonColors(containerColor = Color.LightGray, contentColor = Color.White, disabledContainerColor = Color.DarkGray, disabledContentColor = Color.Yellow),
-            border = BorderStroke(0.dp,Color.Transparent)
+            Image(
+                painter = painterResource(id = R.drawable.sky_high_watchers_logo),
+                modifier = Modifier.fillMaxWidth(),
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth,
+                )
+            
+        }//end of logo image box
+        Spacer(modifier = Modifier.height(10.dp))
+        //box for menu buttons
+        Box (
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.7f)
+                .background(Color.White)
+        ){
+            //icon button for home
+            IconButton(
+                onClick = {
+                    navController.navigate("Homepage")
+                },
+                modifier = Modifier
+                    .padding(5.dp)
+                    .background(colorResource(id = R.color.french_grey))
+                    .fillMaxWidth(),
+                colors = IconButtonDefaults.iconButtonColors(colorResource(id = R.color.french_grey))
             ) {
-            Text(text = "Logout")
-        }
-    }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Home,
+                        tint = Color.Blue,
+                        contentDescription = null,
+                        modifier = Modifier.padding(5.dp)
+                    )
+                    Text(text = "Home", fontWeight = FontWeight.Bold)
+                }
+            }//end of icon button for home
+
+            //icon button for collection
+            IconButton(
+                onClick = {
+                    navController.navigate("collection")
+                },
+                modifier = Modifier
+                    .padding(5.dp, 70.dp)
+                    .background(colorResource(id = R.color.french_grey))
+                    .fillMaxWidth(),
+                colors = IconButtonDefaults.iconButtonColors(colorResource(id = R.color.french_grey))
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.AddCircle,
+                        tint = Color.Blue,
+                        contentDescription = null,
+                        modifier = Modifier.padding(5.dp)
+                    )
+                    Text(text = "Collection", fontWeight = FontWeight.Bold)
+                }
+            }//end of collection button
+
+            //icon button for profile
+            IconButton(
+                onClick = {
+                    navController.navigate("profile")
+                },
+                modifier = Modifier
+                    .padding(5.dp, 140.dp)
+                    .background(colorResource(id = R.color.french_grey))
+                    .fillMaxWidth(),
+                colors = IconButtonDefaults.iconButtonColors(colorResource(id = R.color.french_grey))
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Person,
+                        tint = Color.Blue,
+                        contentDescription = null,
+                        modifier = Modifier.padding(5.dp)
+                    )
+                    Text(text = "Profile", fontWeight = FontWeight.Bold)
+                }
+            }//end of profile icon button
+
+            //icon button for settings
+            IconButton(
+                onClick = {
+                    navController.navigate("settings")
+                },
+                modifier = Modifier
+                    .padding(5.dp, 210.dp)
+                    .background(colorResource(id = R.color.french_grey))
+                    .fillMaxWidth(),
+                colors = IconButtonDefaults.iconButtonColors(colorResource(id = R.color.french_grey))
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Settings,
+                        tint = Color.Blue,
+                        contentDescription = null,
+                        modifier = Modifier.padding(5.dp)
+                    )
+                    Text(text = "Settings", fontWeight = FontWeight.Bold)
+                }
+            }//end of icon button for settings
+
+            //line divider
+            HorizontalDivider(modifier = Modifier.padding(0.dp, 500.dp, 0.dp, 0.dp).border(2.dp, Color.Black))
+
+            //log out icon button
+            IconButton(
+                onClick = {
+                    navController.navigate("logout")
+                },
+                modifier = Modifier
+                    .padding(5.dp, 520.dp, 5.dp, 0.dp)
+                    .background(colorResource(id = R.color.red))
+                    .fillMaxWidth(),
+                colors = IconButtonDefaults.iconButtonColors(colorResource(id = R.color.red))
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.ExitToApp,
+                        tint = Color.Black,
+                        contentDescription = null,
+                        modifier = Modifier.padding(5.dp, 5.dp)
+                    )
+                    Text(text = "Logout", fontWeight = FontWeight.Bold, color = Color.White)
+                }
+            }
+        }//end of box two
+
+
+    }//end of main column
+
 }
+
 @SuppressLint("ResourceAsColor")
 @Composable
-fun Homepage(){
+fun Homepage() {
     var enabled by remember { mutableStateOf(true) }
 
-    Scaffold {PaddingValue ->
+    Scaffold { paddingValues ->
         Column(
             modifier = Modifier.fillMaxSize()
         ) {
@@ -234,7 +310,7 @@ fun Homepage(){
                     .fillMaxWidth()
                     .background(Color(dark_blue))
                     .height(230.dp)
-                    .padding(PaddingValue),
+                    .padding(paddingValues),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
@@ -255,7 +331,7 @@ fun Homepage(){
                         )
                         Text(text = "Bird Name")
                         LazyRow {
-                            items(3){
+                            items(3) {
                                 if (!enabled) return@items
                                 InputChip(
                                     onClick = {
