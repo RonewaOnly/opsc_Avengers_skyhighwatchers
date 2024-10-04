938b7301d5ec9714285cd0dff3eb47ab7f32b6ef
ronewa <ronewamaselesele200@gmail.com>
2024-10-04 22:31:09 +0200

    Changed the string value to double

--- app/src/main/java/com/example/skyhigh_prototype/Data/Location.kt
+++ app/src/main/java/com/example/skyhigh_prototype/Data/Location.kt
@@ -2,6 +2,6 @@ package com.example.skyhigh_prototype.Data
 
 // Object used to store the location where birds are found
 data object Location {
-    var LONGITUDE = ""
-    var LATITUDE = ""
+    var LONGITUDE = 0.0
+    var LATITUDE = 0.0
 }
