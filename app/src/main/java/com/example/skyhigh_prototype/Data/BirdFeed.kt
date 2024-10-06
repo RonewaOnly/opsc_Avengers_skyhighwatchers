42e272f9e04ffd22a522c2240125ae178f14953b
ronewa <ronewamaselesele200@gmail.com>
2024-10-06 14:51:23 +0200

    Made some modification in the Data class

--- app/src/main/java/com/example/skyhigh_prototype/Data/BirdFeed.kt
+++ app/src/main/java/com/example/skyhigh_prototype/Data/BirdFeed.kt
@@ -5,9 +5,9 @@ package com.example.skyhigh_prototype.Data
 data class BirdFeed(
     val feed_id: Int = 0,
     val feed_name: String,
-    val feed_grown: List<String> = emptyList(),
-    val description: List<String>,
-    val species_specific: List<String>,
+    val feed_grown: String = "",
+    val description: List<String> = emptyList(),
+    val species_specific: List<String> = emptyList(),
     val feed_images: List<String> = emptyList(),
     val location: List<Location> = emptyList(),
 )
