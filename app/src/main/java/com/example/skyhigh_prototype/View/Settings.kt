638e1617d364facffbe1facf90104347d0cf113c
ST10061533 <105481917+ST10061533@users.noreply.github.com>
2024-10-21 00:21:27 +0200

    Design changes plus a button to delete account

--- app/src/main/java/com/example/skyhigh_prototype/View/Settings.kt
+++ app/src/main/java/com/example/skyhigh_prototype/View/Settings.kt
@@ -1,22 +1,28 @@
 package com.example.skyhigh_prototype.View
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonColors
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material3.ButtonElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
@@ -27,13 +33,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.app.NotificationCompat
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
@@ -62,13 +71,18 @@ fun NavOption(isDarkTheme: Boolean, onThemeChange: (Boolean) -> Unit) {
         composable("App_info") {
             navigateToAppInfo(rememberNav)
         }
+        composable(route = "DeleteUserAccount") {
+            DeleteUserAccount()
+        }
     }
 }
 
 @Composable
 fun Settings(navController: NavController) {
     Column(
-        modifier = Modifier.fillMaxSize().padding(top = 60.dp)
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 120.dp)
     ) {
         OutlinedButton(
             onClick = {
@@ -114,67 +128,124 @@ fun Settings(navController: NavController) {
 }
 
 // General Settings Screen
+@SuppressLint("ComposableNaming")
 @Composable
-fun navigateToGeneralSettings(navController: NavController, isDarkTheme: Boolean, onThemeChange: (Boolean) -> Unit) {
+fun navigateToGeneralSettings(
+    navController: NavController, isDarkTheme: Boolean, onThemeChange: (Boolean) -> Unit
+) {
     val context = LocalContext.current
     var notificationsEnabled by remember { mutableStateOf(true) }
     //variable for metric system change
-    var useKilometers by remember {mutableStateOf(true)}
+    var useKilometers by remember { mutableStateOf(true) }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(top = 60.dp)
+            .padding(start = 20.dp, top = 120.dp)
     ) {
+
         // Notification Settings Section
-        Text(text = stringResource(R.string.notification_settings), style = MaterialTheme.typography.titleSmall)
-        Switch(
-            checked = notificationsEnabled,
-            onCheckedChange = {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp, vertical = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Text(
+                text = stringResource(R.string.notification_settings),
+                style = MaterialTheme.typography.titleSmall,
+                modifier = Modifier.weight(1f)
+            )
+            Switch(checked = notificationsEnabled, onCheckedChange = {
                 notificationsEnabled = it
                 if (notificationsEnabled) {
                     createNotificationChannel(context)
-                    sendNotification(context, context.getString(R.string.app_name), "Notifications enabled!")
+                    sendNotification(
+                        context, context.getString(R.string.app_name), "Notifications enabled!"
+                    )
                 } else {
-                    sendNotification(context, context.getString(R.string.app_name), "Notifications disabled!")
+                    sendNotification(
+                        context, context.getString(R.string.app_name), "Notifications disabled!"
+                    )
                 }
-            }
-        )
-        Spacer(modifier = Modifier.height(20.dp))
+            })
+        }
 
+
+        //distance section
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp, vertical = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Distance unit section : Metric or imperial
+            Text(
+                text = "Distance",
+                style = MaterialTheme.typography.titleSmall,
+                modifier = Modifier.weight(2.8f)
+            )
+
+            Text(
+                text = if (useKilometers) "Kilometers" else "Miles",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.weight(1f)
+            )
+
+            Switch(checked = useKilometers, onCheckedChange = { useKilometers = it })
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
         // Theme Settings Section
-        Text(text = stringResource(R.string.theme_settings), style = MaterialTheme.typography.titleSmall)
+        Text(
+            text = stringResource(R.string.theme_settings),
+            style = MaterialTheme.typography.titleSmall
+        )
+        Spacer(modifier = Modifier.height(10.dp))
         OutlinedButton(
             onClick = {
                 onThemeChange(!isDarkTheme)  // Toggle the theme between Light and Dark
-            },
-            modifier = Modifier.fillMaxWidth()
+            }, modifier = Modifier.fillMaxWidth()
         ) {
             Text(
-                text = if (isDarkTheme) stringResource(R.string.switch_to_light_mode) else stringResource(R.string.switch_to_dark_mode),
-                textAlign = TextAlign.Center
+                text = if (isDarkTheme) stringResource(R.string.switch_to_light_mode) else stringResource(
+                    R.string.switch_to_dark_mode
+                ), textAlign = TextAlign.Center
             )
         }
         Spacer(modifier = Modifier.height(20.dp))
 
-        // Distance unit section : Metric or imperial
-        Text(text = "Distance", style = MaterialTheme.typography.titleSmall)
-        Switch(
-            checked = useKilometers,
-            onCheckedChange = { useKilometers = it }
-        )
-        Text(
-            text = if (useKilometers) "Kilometers" else "Miles",
-            style = MaterialTheme.typography.bodyMedium
-        )
-        Spacer(modifier = Modifier.height(20.dp))
-
         // Language Settings Section
-        Text(text = stringResource(R.string.language_settings), style = MaterialTheme.typography.titleSmall)
+        Text(
+            text = stringResource(R.string.language_settings),
+            style = MaterialTheme.typography.titleSmall
+        )
+        Spacer(modifier = Modifier.height(10.dp))
         LanguageSelection(onLanguageSelected = { selectedLanguage ->
             updateAppLocale(context, selectedLanguage)
-            sendNotification(context, "Language Changed", "App language changed to $selectedLanguage")
+            sendNotification(
+                context, "Language Changed", "App language changed to $selectedLanguage"
+            )
         })
+
+        Spacer(modifier = Modifier.height(20.dp))
+        //delete account section
+        Button(
+            onClick = {
+
+                //navigate to delete account function
+                navController.navigate("DeleteUserAccount")
+            },
+            modifier = Modifier
+                .background(colorResource(id = R.color.red))
+                .width(250.dp)
+                .align(Alignment.CenterHorizontally),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red)
+        ) {
+            Text(text = "Delete Account", fontSize = 20.sp, color = Color.White)
+        }
+
     }
 }
 
@@ -187,16 +258,12 @@ fun LanguageSelection(onLanguageSelected: (String) -> Unit) {
 
     Box(modifier = Modifier.fillMaxWidth()) {
         OutlinedButton(
-            onClick = { expanded = !expanded },
-            modifier = Modifier.fillMaxWidth()
+            onClick = { expanded = !expanded }, modifier = Modifier.fillMaxWidth()
         ) {
             Text(text = "Change Language: $selectedLanguage", textAlign = TextAlign.Center)
         }
 
-        DropdownMenu(
-            expanded = expanded,
-            onDismissRequest = { expanded = false }
-        ) {
+        DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
             DropdownMenuItem(onClick = {
                 selectedLanguage = "English"
                 expanded = false
@@ -243,13 +310,12 @@ fun createNotificationChannel(context: Context) {
 
 // Function to send notifications
 fun sendNotification(context: Context, title: String, message: String) {
-    val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-    val notification = NotificationCompat.Builder(context, "birdwatching_notifications")
-        .setContentTitle(title)
-        .setContentText(message)
-        .setSmallIcon(R.drawable.ic_notification)
-        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-        .build()
+    val notificationManager =
+        context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    val notification =
+        NotificationCompat.Builder(context, "birdwatching_notifications").setContentTitle(title)
+            .setContentText(message).setSmallIcon(R.drawable.ic_notification)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT).build()
     notificationManager.notify(1, notification)
 }
 
@@ -260,15 +326,15 @@ fun navigateToCustomAreas(navController: NavController) {
     var newAreaName by remember { mutableStateOf("") }
 
     Column(
-        modifier = Modifier.fillMaxSize().padding(top = 60.dp)
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 60.dp)
     ) {
         Text(text = stringResource(R.string.define_custom_areas))
 
-        OutlinedTextField(
-            value = newAreaName,
+        OutlinedTextField(value = newAreaName,
             onValueChange = { newAreaName = it },
-            label = { Text(stringResource(R.string.new_area_name)) }
-        )
+            label = { Text(stringResource(R.string.new_area_name)) })
 
         OutlinedButton(onClick = {
             if (newAreaName.isNotBlank()) {
@@ -301,15 +367,15 @@ fun navigateToReport(navController: NavController) {
     var newReport by remember { mutableStateOf("") }
 
     Column(
-        modifier = Modifier.fillMaxSize().padding(top = 60.dp)
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 60.dp)
     ) {
         Text(text = stringResource(R.string.report_bird_sighting))
 
-        OutlinedTextField(
-            value = newReport,
+        OutlinedTextField(value = newReport,
             onValueChange = { newReport = it },
-            label = { Text(stringResource(R.string.enter_bird_details)) }
-        )
+            label = { Text(stringResource(R.string.enter_bird_details)) })
 
         OutlinedButton(onClick = {
             if (newReport.isNotBlank()) {
@@ -339,7 +405,9 @@ fun navigateToReport(navController: NavController) {
 @Composable
 fun navigateToAppInfo(navController: NavController) {
     Column(
-        modifier = Modifier.fillMaxSize().padding(top = 60.dp)
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 60.dp)
     ) {
         Text(text = "App Version: 1.0.0")
         Text(text = "Developed by: Birdwatching Team")
