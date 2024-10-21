4ebf3d3a6db60f282c86ee87193ab719c31ebb74
ronewa <ronewamaselesele200@gmail.com>
2024-10-21 21:12:50 +0200

    Made some modification to the attributes

--- app/src/main/java/com/example/skyhigh_prototype/Data/UserDetails.kt
+++ app/src/main/java/com/example/skyhigh_prototype/Data/UserDetails.kt
@@ -1,15 +1,15 @@
 package com.example.skyhigh_prototype.Data
 
 data class UserDetails(
-    var firstname: String,
-    var lastname: String,
-    var email: String,
-    var password: String,
+    var firstname: String? = "",
+    var lastname: String? = "",
+    var email: String? ="",
     val bio: String? ="",
     val location: String? = "",
     var settings: List<Settings?> = emptyList(),
     var customArea: List<CustomArea?> = emptyList(),
-    val favoriteBirds: List<String> = emptyList(),
+    var sightingsCount:Double = 0.0,
+    var favoriteBirds: List<String> = emptyList(),
     var reports: List<Reports?> = emptyList(),
     var achievements: List<Achievement?> = emptyList(),
     var profilePic: String?= "https://th.bing.com/th/id/R.7861dadc7807c489531e672f28d0ecec?rik=uqbbCnsZ0OUbjw&pid=ImgRaw&r=0"
